e99cacfaa9b09979db3f63b0eef381de038facb8
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-06-07 17:09:47 +0530

    Update music.py

--- SaitamaRobot/modules/music.py
+++ SaitamaRobot/modules/music.py
@@ -14,7 +14,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@pbot.on_message(filters.command(['song']))
+# @pbot.on_message(filters.command(['song']))
 def song(client, message):
 
     user_id = message.from_user.id 
