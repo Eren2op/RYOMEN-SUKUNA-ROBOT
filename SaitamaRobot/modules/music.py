9ddd69f1febfffa343dcafa15ba310c7045c99d5
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-06-07 15:56:56 +0530

    Update music.py

--- SaitamaRobot/modules/music.py
+++ SaitamaRobot/modules/music.py
@@ -30,7 +30,7 @@ def song(client, message):
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
-        #print(results)
+        print(results)
         title = results[0]["title"][:40]       
         thumbnail = results[0]["thumbnails"][0]
         thumb_name = f'thumb{title}.jpg'
