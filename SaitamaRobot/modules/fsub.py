a76186af50351e18f99bd925bc9534fac0a61b3f
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-06-13 18:30:54 +0530

    Update fsub.py

--- SaitamaRobot/modules/fsub.py
+++ SaitamaRobot/modules/fsub.py
@@ -215,7 +215,7 @@ def config(client, message):
 
 
 __help__ = """
-<b>ForceSubscribe:</b>
+<b>FSub:</b>
 - Pain can mute members who are not subscribed your channel until they subscribe
 - When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 <b>Setup</b>
