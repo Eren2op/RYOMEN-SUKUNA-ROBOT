cfd7c92d7da8775d5cc5f2729a05e9480dcf813e
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-06-14 21:12:31 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -84,7 +84,7 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER',
                                  'CAADAgADOwADPPEcAXkko5EB3YGYAg')
-    GBAN_STICKER = os.environ.get('GBAN_STICKER',
+    WARN_STICKER = os.environ.get('WARN_STICKER',
                                  'CAACAgIAAx0CR5EMzQACG4Jgx22OYBPSg9uziHl38zPYhqfTCAAC0QADpyOPCQ60d6I7uPTdHwQ')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
@@ -157,7 +157,7 @@ else:
     STRICT_GBAN = Config.STRICT_GBAN
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
-    GBAN_STICKER = Config.GBAN_STICKER
+    WARN_STICKER = Config.WARN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
