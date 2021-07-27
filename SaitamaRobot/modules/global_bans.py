f98631b5e34e256b5624592780383eb17c2ac8ce
Eren2op <87973721+Eren2op@users.noreply.github.com>
2021-07-27 21:14:19 +0530

    Update global_bans.py

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from io import BytesIO
 from SaitamaRobot.modules.sql.users_sql import get_user_com_chats
 import SaitamaRobot.modules.sql.global_bans_sql as sql
-from SaitamaRobot import (BAN_STICKER, DEV_USERS, EVENT_LOGS, OWNER_ID, STRICT_GBAN, DRAGONS,
+from SaitamaRobot import (BAN_STICKER, DEV_USERS, EVENT_LOGS, UNGBAN_GIF, OWNER_ID, STRICT_GBAN, DRAGONS,
                           SUPPORT_CHAT, SPAMWATCH_SUPPORT_CHAT, DEMONS, TIGERS,
                           WOLVES, sw, dispatcher)
 from SaitamaRobot.modules.helper_funcs.chat_status import (is_user_admin,
@@ -269,8 +269,8 @@ def ungban(update: Update, context: CallbackContext):
         return
 
     message.reply_text(
-        f"I'll give {user_chat.first_name} a second chance, globally.")
-    bot.send_sticker(chat.id, BAN_STICKER)
+        f"I'll Allow It Once {user_chat.first_name} There Won't Be A Second Time.")
+    bot.send_sticker(chat.id, UNGBAN_GIF)
 
     start_time = time.time()
     datetime_fmt = "%Y-%m-%dT%H:%M"
