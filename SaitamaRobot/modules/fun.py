4f422d6e62deec1d1ebfc0ed6416aa9f40201ddf
Eren2op <87973721+Eren2op@users.noreply.github.com>
2021-07-29 23:07:57 +0530

    Update fun.py

--- SaitamaRobot/modules/fun.py
+++ SaitamaRobot/modules/fun.py
@@ -47,11 +47,11 @@ def runs(update: Update, context: CallbackContext):
 
 
 @run_async
-def sanitize(update: Update, context: CallbackContext):
+def extension(update: Update, context: CallbackContext):
     message = update.effective_message
     name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
     reply_animation = message.reply_to_message.reply_animation if message.reply_to_message else message.reply_animation
-    reply_animation(GIF_ID, caption=f'YOU DARE TOUCH MY SOUL?')
+    reply_animation(DARE_GIF), caption=f'YOU DARE TOUCH MY SOUL?')
 
 
 @run_async
@@ -67,7 +67,7 @@ def sanitize(update: Update, context: CallbackContext):
     message = update.effective_message
         name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
             reply_animation = message.reply_to_message.reply_animation if message.reply_to_message else message.reply_animation
-                reply_animation(DARE_GIF), caption=f'*Sanitizes {name}*')
+                reply_animation(GIF_ID), caption=f'*Sanitizes {name}*')
 
 
 @run_async
